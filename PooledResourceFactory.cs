9c0e35e1f5674dc89fe177898051295d4d75b0d8
zhufamily <ning.zhu.1973@gmail.com>
2025-05-12 15:55:37 -0400

    Add generic resource management class, and another derided class for ServiceClient

---
+++ PooledResourceFactory.cs
@@ -0,0 +1,185 @@
+﻿using System.Collections.Concurrent;
+
+namespace Ning.Sample
+{
+    public interface IPooledResourceFactory<T>
+    { 
+        public T Acquire();
+        public void Release(T resource);
+        public int TotalCapacity();
+        public int AvailableCapacity();
+    }
+
+    public class PooledResourceFactory<T> : IPooledResourceFactory<T>, IDisposable
+    {
+        private int _initialCapacity, _maxCapacity, _step, _intervalInMinutes, _prevIntervals, _currentCapacity;
+        private int [] _lowestResourcesAvailable;
+        private BlockingCollection<T> _resources = new BlockingCollection<T>();
+        private Timer _timer;
+        private object _lock = new object();
+        private bool _disposed = false;
+        private Func<T> _factory;
+
+        public PooledResourceFactory(Func<T> func, int initialCapacity = 16, int maxCapacity = 64, int step = 8, 
+            int intervalInMinutes = 5, int prevIntervals = 3)
+        {
+            _initialCapacity = initialCapacity;
+            _currentCapacity = initialCapacity;
+            _maxCapacity = maxCapacity;
+            _step = step;
+            _intervalInMinutes = intervalInMinutes;
+            _prevIntervals = prevIntervals;
+            _factory = func;
+
+            for (int i = 0; i < _initialCapacity; i++)
+            {
+                T resource = _factory();
+                _resources.Add(resource);
+            }
+            _lowestResourcesAvailable = new int[_prevIntervals];
+            for (int i = 0; i < _prevIntervals - 1; i++)
+            {
+                _lowestResourcesAvailable[i] = -1;
+            }
+            _lowestResourcesAvailable[_prevIntervals - 1] = _initialCapacity;
+            _timer = new Timer(ScaleResource, null, TimeSpan.FromMinutes(_intervalInMinutes), TimeSpan.FromMinutes(_intervalInMinutes));
+        }
+
+        private void ScaleOut()
+        {
+            for (int i = 0; i < _step; i++)
+            {
+                if (_currentCapacity >= _maxCapacity)
+                {
+                    break;
+                }
+                
+                T newResource = _factory();
+                _resources.Add(newResource);
+                _currentCapacity++;
+            }
+        }
+
+        private void ScaleIn()
+        {
+            for (int i = 0; i < _step; i++)
+            {
+                if (_resources.TryTake(out T? discardResource))
+                {
+                    if (discardResource != null && discardResource is IDisposable)
+                    {
+                        ((IDisposable)discardResource).Dispose();
+                    }
+                    _currentCapacity--;
+                }
+                else
+                {
+                    break;
+                }
+            }
+        }
+
+        private void ScaleResource(object? state = null)
+        {
+            lock (_lock)
+            {
+                if (_lowestResourcesAvailable.All(x => x > _step) && _currentCapacity - _initialCapacity > _step)
+                {
+                    ScaleIn();
+                }
+                else if (_lowestResourcesAvailable.All(x => (x < 2) && (x >= 0)) && _currentCapacity < _maxCapacity)
+                {
+                    ScaleOut();
+                }
+
+                for (int i = 1; i < _prevIntervals; i++)
+                {
+                    _lowestResourcesAvailable[i - 1] = _lowestResourcesAvailable[i];
+                }
+                _lowestResourcesAvailable[_prevIntervals - 1] = _resources.Count;
+                return;   
+            }
+        }
+
+        public T Acquire()
+        {
+            T? resource;
+            // If resource available, return immediately
+            if (_resources.TryTake(out resource))
+            {
+                if (_resources.Count < _lowestResourcesAvailable[_prevIntervals - 1])
+                {
+                    _lowestResourcesAvailable[_prevIntervals - 1] = _resources.Count;
+                }
+                return resource;
+            }
+
+            // Increase capacity, if not yet reaching max capacity
+            // Increase resource capacity by step
+            if (Monitor.TryEnter(_lock))
+            {
+                try
+                {
+                    ScaleOut();
+                }
+                finally 
+                { 
+                    Monitor.Exit(_lock); 
+                }
+            }
+            
+            // Wait for next available resource
+            resource = _resources.Take();
+            _lowestResourcesAvailable[_prevIntervals - 1] = _resources.Count;
+            return resource;
+        }
+
+        public void Release(T resource)
+        {
+            _resources.Add(resource);
+        }
+
+        public int TotalCapacity()
+        {
+            return _currentCapacity;
+        }
+
+        public int AvailableCapacity()
+        {
+            return _resources.Count;
+        }
+
+        #region IDispose
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+
+        // Protected implementation of Dispose pattern.
+        protected virtual void Dispose(bool disposing)
+        {
+            if (!_disposed)
+            {
+                _disposed = true;
+
+                if (disposing)
+                {
+                    // Release all resources in the pool
+                    // If loaned out resources are NOT returned to the pool
+                    // Assuming those are released elsewhere
+                    foreach (T resource in _resources)
+                    {
+                        if (resource != null && resource is IDisposable)
+                        {
+                            ((IDisposable)resource).Dispose();
+                        }
+                    }
+                    _resources.Dispose();
+                    _timer.Dispose();
+                }
+            }
+        }
+        #endregion
+    }
+}
--- PooledServiceClientFactory.cs
+++ PooledServiceClientFactory.cs
@@ -1,206 +1,29 @@
 ﻿using Microsoft.PowerPlatform.Dataverse.Client;
-using System.Collections.Concurrent;
 
 namespace Ning.Sample
 {
     /// <summary>
     /// Public interface for Service Client pool
     /// </summary>
-    public interface IPooledServiceClientFactory
+    public interface IPooledServiceClientFactory : IPooledResourceFactory<ServiceClient>
     {
-        /// <summary>
-        /// Get / loan a resource from the pool
-        /// </summary>
-        /// <returns></returns>
-        public ServiceClient Acquire();
-        /// <summary>
-        /// Return loaned resource to the pool
-        /// </summary>
-        /// <param name="serviceClient"></param>
-        public void Release(ServiceClient serviceClient);
-        /// <summary>
-        /// Current pool capacity
-        /// </summary>
-        /// <returns></returns>
-        public int TotalCapacity();
-        /// <summary>
-        /// Current available resources in the pool
-        /// </summary>
-        /// <returns></returns>
-        public int AvailableCapacity();
     }
 
     /// <summary>
     /// Main class for Service Client pool
     /// </summary>
-    public class PooledServiceClientFactory : IPooledServiceClientFactory, IDisposable
+    public class PooledServiceClientFactory : PooledResourceFactory<ServiceClient>, IPooledServiceClientFactory
     {
-        #region Private Members
-        private string _dataverseConnectionString;
-        private BlockingCollection<ServiceClient> _serviceClientPool;
-        private bool _disposed = false;
-        private object _lock = new object();
-        private int _initialCapacity;
-        private int _currentCapacity;
-        private int _maxCapacity;
-        private int _step;
-        private int _lowestAvailableCapacity;
-        private readonly Timer _timer;
-        #endregion
-
         /// <summary>
         /// Public constructor
         /// </summary>
-        public PooledServiceClientFactory(string dataverseConnectionString, int capacity = 16)
+        public PooledServiceClientFactory(string dataverseConnectionString) : base(() => { 
+            ServiceClient serviceClient = new ServiceClient(dataverseConnectionString);
+            serviceClient.UseWebApi = true;
+            serviceClient.DisableCrossThreadSafeties = true;
+            return serviceClient;
+        })
         {
-            // Init private members
-            _dataverseConnectionString = dataverseConnectionString;
-            _serviceClientPool = new BlockingCollection<ServiceClient>();
-            _initialCapacity = capacity;
-            _currentCapacity = capacity;
-            _maxCapacity = capacity * 4;
-            _step = _currentCapacity / 2 > 1 ? _currentCapacity / 2 : 1;
-            _lowestAvailableCapacity = _currentCapacity;
-            for (int i = 0; i < _currentCapacity; i++)
-            {
-                ServiceClient client = new ServiceClient(_dataverseConnectionString);
-                client.UseWebApi = true;
-                client.DisableCrossThreadSafeties = true;
-                _serviceClientPool.Add(client);
-            }
-            _timer = new Timer(ScaleBack, null, TimeSpan.FromMinutes(30), TimeSpan.FromMinutes(30));
         }
-
-        /// <summary>
-        /// Scale back -- check every 30 minutes
-        /// </summary>
-        /// <param name="state"></param>
-        private void ScaleBack(object? state = null)
-        {
-            lock (_lock)
-            {
-                if (_currentCapacity - _initialCapacity >= _step && _lowestAvailableCapacity >= _step)
-                {
-                    for (int i = 0; i < _step; i++)
-                    {
-                        if (_serviceClientPool.TryTake(out ServiceClient? discardClient))
-                        {
-                            discardClient.Dispose();
-                            _currentCapacity--;
-                        }
-                        else
-                        {
-                            break;
-                        }
-                    }
-                }
-                _lowestAvailableCapacity = _serviceClientPool.Count;
-            }
-        }
-
-        /// <summary>
-        /// Acquire a service client available
-        /// Only for scoped variable
-        /// Never used as shared or static variable
-        /// </summary>
-        /// <returns></returns>
-        public ServiceClient Acquire()
-        {
-            // If resource available, return immediately
-            if (_serviceClientPool.TryTake(out ServiceClient? resource))
-            {
-                if (_serviceClientPool.Count < _lowestAvailableCapacity)
-                {
-                    _lowestAvailableCapacity = _serviceClientPool.Count;
-                }
-                return resource;
-            }
-
-            // Increase capacity, if not yet reaching max capacity
-            // Increase resource capacity by step
-            lock (_lock)
-            {
-                int count = 0;
-                while (count < _step && _currentCapacity < _maxCapacity)
-                {
-                    ServiceClient client = new ServiceClient(_dataverseConnectionString);
-                    client.UseWebApi = true;
-                    client.DisableCrossThreadSafeties = true;
-                    _serviceClientPool.Add(client);
-                    _currentCapacity++;
-                    count++;
-                }
-            }
-
-            // Wait for next available resource
-            ServiceClient scopedClient = _serviceClientPool.Take();
-            if (_serviceClientPool.Count < _lowestAvailableCapacity)
-            {
-                _lowestAvailableCapacity = _serviceClientPool.Count;
-            }
-            return scopedClient;
-        }
-
-        /// <summary>
-        /// Put a service client back to pool
-        /// Only if loaded out a resource from the pool
-        /// Never put a newly created resource into the pool
-        /// </summary>
-        /// <param name="serviceClient"></param>
-        public void Release(ServiceClient serviceClient)
-        {
-            _serviceClientPool.Add(serviceClient);
-        }
-
-        /// <summary>
-        /// Get current resource count
-        /// Not resources available, but total resources in recycling
-        /// </summary>
-        /// <returns></returns>
-        public int TotalCapacity()
-        {
-            return _currentCapacity;
-        }
-
-        /// <summary>
-        /// Get current available resource count
-        /// Not total resources in recycling but resources available
-        /// </summary>
-        /// <returns></returns>
-        public int AvailableCapacity()
-        {
-            return _serviceClientPool.Count;
-        }
-
-        #region IDispose
-        public void Dispose()
-        {
-            Dispose(true);
-            GC.SuppressFinalize(this);
-        }
-
-        // Protected implementation of Dispose pattern.
-        protected virtual void Dispose(bool disposing)
-        {
-            if (!_disposed)
-            {
-                _disposed = true;
-
-                if (disposing)
-                {
-                    // Release all resources in the pool
-                    // If loaned out resources are NOT returned to the pool
-                    // Assuming those are released elsewhere
-                    foreach (ServiceClient client in _serviceClientPool)
-                    {
-                        client.Dispose();
-                    }
-                    _serviceClientPool.Dispose();
-                    _timer.Dispose();
-                }
-            }
-        }
-
-        #endregion
     }
 }
